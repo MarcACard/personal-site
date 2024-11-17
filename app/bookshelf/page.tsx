dde833080501b68bb2267935117dab5a63737497
Marc Anthony Card <marcacard@gmail.com>
2024-11-17 17:34:42 -0500

    wip: my bookshelf

---
+++ app/bookshelf/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: 'Bookshelf',
+    description: '',
+  }
+  
+  export default function Page() {
+    return (
+      <section>
+        <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Bookshelf</h1>
+        <p className="mb-12 italic">Coming Soon...</p>
+      </section>
+    )
+  }
+  
--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -7,6 +7,9 @@ const navItems = {
   '/writing': {
     name: 'Writing',
   },
+  '/bookshelf': {
+    name: 'Bookshelf',
+  },
 }
 
 export function Navbar() {
